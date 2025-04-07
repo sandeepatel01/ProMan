3111abcb1bf5655e937b7b32af9926a642d60178
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-04-07 11:09:22 +0530

    App Testing

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 // Routers Imports
-import { healthcheck } from "./controllers/healthcheck.controllers";
+import { healthcheck } from "./controllers/healthcheck.controllers.js";
 
 app.use("/api/v1/healthcheck", healthcheck);
 
